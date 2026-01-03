3a5b7f5cb2c6ffa6c63a37ed9bc206ea3188bb47
Matteus Barbosa <matteus.barbosa@gmail.com>
2026-01-03 13:58:34 -0300

    Developed the function

--- src/reject_null_hipotesys_t_test.py
+++ src/reject_null_hipotesys_t_test.py
@@ -4,7 +4,6 @@ This module provides hipotesys testing analysis for a paremeter.
 This module does uses third-party modules.
 """
 
-import numpy as np
 from scipy.stats import t
 
 
@@ -13,9 +12,19 @@ def reject_null_hipotesys_t_test(
         alpha,
         degrees_of_freedom,
         standard_error
-):
+) -> dict:
     
     # Calculates the p-value.
     t_stats = (parameter - 0) / (standard_error)
     p_value = 2 * (1 - t.cdf(abs(t_stats), degrees_of_freedom))
     
+    # Compares the value of p-value with alpha.
+    if p_value <= alpha:
+        test_result = {
+            "reject_null_hypotesis": True
+            }
+    else:
+        test_result = {
+            "reject_null_hypotesis": False
+            }
+    return test_result
